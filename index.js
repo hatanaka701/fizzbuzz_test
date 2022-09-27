2d49e9e0d489ecb3ec605c9726c9e1faf9f7c3e4
humanmodeling OIT <humanmodelingoit@humanmodelingnoMacBook-Air.local>
2022-09-27 16:02:47 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let input = document.getElementById('input');
+let btn = document.getElementById('btn');
+let result = document.getElementById('result');
+
+btn.addEventListener('click', () => {
+    if (!Number(input.value)) {
+        result.innerText = '数値を入力してください';
+    } else if (Number(input.value) % 2 === 0) {
+        result.innerText = '偶数';
+    } else {
+        result.innerText = '奇数';
+    }
+});
