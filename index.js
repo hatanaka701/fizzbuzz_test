926c9cbf1efab96d22d4fa3b62ebd70b1ae62806
humanmodeling OIT <humanmodelingoit@humanmodelingnoMacBook-Air.local>
2022-09-27 16:21:22 +0900

    init test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let result = document.getElementById('result');
 
 btn.addEventListener('click', () => {
     if (!Number(input.value)) {
-        result.innerText = '数値を入力しなさい！';
+        result.innerText = '数値を入力しなさい！あほ';
     } else if (Number(input.value) % 2 === 0) {
         result.innerText = '偶数';
     } else {
